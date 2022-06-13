d8330088f90e1af39d55d5d1dd8ff1f561f06f11
hariomsuryvanshi <hariom.s@simformsolutions.com>
2022-06-13 15:28:15 +0530

    final commit on newsapp

--- myapp/tasks.py
+++ myapp/tasks.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 import threading
+import pyshorteners
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
@@ -107,244 +108,25 @@ def task_mail(self):
             except:
                 art=None  
             if art==None:
-                article_img_url=article['urlToImage']
-                article_url=article['url']
-                article_title = article['title']
-                print(len(article_title))
-                if article_url:
-                    if len(article_url)>250:
-                        shortener=pyshorteners.Shortener()
-                        x = shortener.tinyurl.short(article_url)
-                        article_url = x
-
-                if article_img_url:
-                    if len(article_img_url)>200:
-                        shortener=pyshorteners.Shortener()
-                        x = shortener.tinyurl.short(article_img_url)
-                        article_img_url = x    
-
                 article_instance=Articles(
-                    article_img_url=article_img_url,
+                    article_img_url=article['urlToImage'],
                     article_author=article['author'],
-                    article_title=article['title'],
+                    article_title=article['title'][:250],
                     article_description=article['description'][:249],
-                    article_url=article_url,
+                    article_url=article['url'],
                     article_publish=article['publishedAt'],
                     article_category=article_category[index]
                 )
                 article_instance.save()
-                user=UserProfile.objects.filter(
-                    news_preference_1=article_category[index].lower()).values('user_id__email') 
-                email = []
-                for profile in user:
-                    email.append(profile['user_id__email'])
+                email=UserProfile.objects.filter(
+                    news_preference_1=article_category[index].lower()
+                    ).values_list('user_id__email',flat=True) 
+                email=list(email)    
                 send_mail_func.delay(email, 
                                     health_article['author'],
                                     health_article['title'],
                                     health_article['url'],
                                     article_category[index]
-                                    )
-    return 'Done'                                
-    #     try:
-    #         try:
-    #             art=Articles.objects.get(article_title=health_article['title'])
-    #         except:
-    #             art=None  
-    #         if art==None:
-    #             a=Articles(
-    #                 article_img_url=health_article['urlToImage'],
-    #                 article_author=health_article['author'],
-    #                 article_title=health_article['title'],
-    #                 article_description=health_article['description'],
-    #                 article_url=health_article['url'],
-    #                 article_publish=health_article['publishedAt'],
-    #                 article_category='Health'
-    #             )
-    #             a.save()
-    #             user=UserProfile.objects.filter(
-    #                 news_preference_1='health').values('user_id__email') 
-    #             email = []
-    #             for profile in user:
-    #                 email.append(profile['user_id__email'])
-    #             send_mail_func.delay(email, 
-    #                                 health_article['author'],
-    #                                 health_article['title'],
-    #                                 health_article['url'],
-    #                                 'Health'
-    #                                 )
-    #     except:
-    #         pass
-    #     try:
-    #         try:
-    #             art=Articles.objects.get(article_title=entertainment_article['title'])
-    #         except:
-    #             art=None  
-    #         if art==None:
-    #             a=Articles(
-    #                 article_img_url=entertainment_article['urlToImage'],
-    #                 article_author=entertainment_article['author'],
-    #                 article_title=entertainment_article['title'],
-    #                 article_description=entertainment_article['description'], 
-    #                 article_url=entertainment_article['url'],
-    #                 article_publish=entertainment_article['publishedAt'],
-    #                 article_category='Entertainment'
-    #                 )
-    #             a.save()
-    #             user=UserProfile.objects.filter(
-    #                 news_preference_1='entertainment').values('user_id__email') 
-    #             email = []
-    #             for profile in user:
-    #                 email.append(profile['user_id__email'])
-    #             send_mail_func.delay(
-    #                                 email,
-    #                                 entertainment_article['author'],
-    #                                 entertainment_article['title'],
-    #                                 entertainment_article['url'],
-    #                                 'Entertainment'
-    #                                 )
-    #     except:
-    #         pass
-
-    #     try:
-    #         try:
-    #             art = Articles.objects.get(article_title=general_article['title'])
-    #         except:
-    #             art = None  
-    #         if art==None:
-    #             article=Articles(
-    #                 article_img_url=general_article['urlToImage'],
-    #                 article_author=general_article['author'],
-    #                 article_title=general_article['title'],
-    #                 article_description=general_article['description'],
-    #                 article_url=general_article['url'],
-    #                 article_publish=general_article['publishedAt'], 
-    #                 article_category='General'
-    #                 )
-    #             article.save()
-    #             user=UserProfile.objects.filter(
-    #                 news_preference_1='general').values('user_id__email') 
-    #             email = []
-    #             for profile in user:
-    #                 email.append(profile['user_id__email']) 
-    #             send_mail_func.delay(email,general_article['author'],
-    #                                 general_article['title'],
-    #                                 general_article['url'],
-    #                                 'General'
-    #                                 )
-    #     except:
-    #         pass
-
-    #     try:
-    #         try:
-    #             art=Articles.objects.get(article_title=business_article['title'])
-    #         except:
-    #             art=None  
-    #         if art==None:
-    #             a=Articles(article_img_url=business_article['urlToImage'],
-    #                         article_author=business_article['author'],
-    #                         article_title=business_article['title'], 
-    #                         article_description=business_article['description'],
-    #                         article_url=business_article['url'],
-    #                         article_publish=business_article['publishedAt'],
-    #                         article_category='Business'
-    #                         )
-    #             a.save()
-    #             user=UserProfile.objects.filter(
-    #                 news_preference_1='business').values('user_id__email') 
-    #             email = []
-    #             for profile in user:
-    #                 email.append(profile['user_id__email'])
-    #             send_mail_func.delay(email,
-    #                     business_article['author'],
-    #                     business_article['title'],
-    #                     business_article['url'],
-    #                     'Business'
-    #                     )
-
-    #     except:
-    #         pass
-
-    #     try:
-    #         try:
-    #             art=Articles.objects.get(article_title=sports_article['title'])
-    #         except:
-    #             art=None  
-    #         if art==None:
-    #             a=Articles(article_img_url=sports_articles['urlToImage'],
-    #             article_author=sports_articles['author'], 
-    #             article_title=sports_articles['title'],
-    #             article_description=sports_articles['description'],
-    #             article_url=sports_articles['url'], 
-    #             article_publish=sports_articles['publishedAt'],
-    #             article_category='Sports')
-    #             a.save()
-    #             user=UserProfile.objects.filter(
-    #                 news_preference_1='sports').values('user_id__email') 
-    #             email = []
-    #             for profile in user:
-    #                 email.append(profile['user_id__email'])  
-    #             send_mail_func.delay(email,sports_article['author'],
-    #                     sports_article['title'],sports_article['url'],
-    #                     'Sports'
-    #                     )
-    #     except:
-    #         pass
-
-    #     try:
-    #         try:
-    #             art=Articles.objects.get(article_title=science_article['title'])
-    #         except:
-    #             art=None  
-    #         if art==None:   
-    #             a=Articles(article_img_url=science_articles['urlToImage'],
-    #                         article_author=science_articles['author'], 
-    #                         article_title=science_articles['title'], 
-    #                         article_description=science_articles['description'], 
-    #                         article_url=science_articles['url'], 
-    #                         article_publish=science_articles['publishedAt'], 
-    #                         article_category='Science'
-    #                         )
-    #             a.save()
-    #             user=UserProfile.objects.filter(
-    #                 news_preference_1='science').values('user_id__email') 
-    #             email = []
-    #             for profile in user:
-    #                 email.append(profile['user_id__email'])
-    #             print(email) 
-    #             send_mail_func.delay(email,science_article['author'],
-    #                 science_article['title'],
-    #                 science_article['url'],
-    #                 'Science'
-    #             )
-    #     except:
-    #         pass 
-
-    #     try:
-    #         try:
-    #             art=Articles.objects.get(article_title=technology_article['title'])
-    #         except:
-    #             art=None    
-    #         if art==None:
-    #             a=Articles(
-    #                 article_img_url=technology_articles['urlToImage'],
-    #                 article_author=technology_articles['author'], 
-    #                 article_title=technology_articles['title'], 
-    #                 article_description=technology_articles['description'], 
-    #                 article_url=technology_articles['url'], 
-    #                 article_publish=technology_articles['publishedAt'], 
-    #                 article_category='Technology'
-    #             )
-    #             a.save()
-    #             user=UserProfile.objects.filter(
-    #                 news_preference_1='technology').values('user_id__email') 
-    #             email = []
-    #             for profile in user:
-    #                 email.append(profile['user_id__email'])  
-    #             send_mail_func.delay(email,technology_article['author'],
-    #                                 technology_article['title'],
-    #                                 technology_article['url'],
-    #                                 'Technology'
-    #                                 )
-    #     except:
-    #         pass           
-    # return ('Done Finally')
+                                    )     
+    return 'Done'                        
+    
